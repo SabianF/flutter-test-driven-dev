7c4e5381e10671aaaf599feb6ca615502cf98770
SabianF <58588133+SabianF@users.noreply.github.com>
2021-12-03 18:03:25 -0800

    created HomePage button; tests green

--- lib/screens/root/home_page.dart
+++ lib/screens/root/home_page.dart
@@ -7,6 +7,29 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Hello, friend.');
+    return Center(
+      child: Column(
+        children: <Widget>[
+          const Text('Hello, friend.'),
+
+          // Weather button
+          MaterialButton(
+            onPressed: () {},
+            color: Colors.blue,
+            child: Row(
+              children: const <Widget>[
+
+                Text('Weather today'),
+
+                Icon(
+                  Icons.wb_sunny,
+                  key: Key('icon_weather'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      )
+    );
   }
 }
