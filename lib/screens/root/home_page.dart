10906ab3c22acce983f5a491d8953a461e99c681
SabianF <58588133+SabianF@users.noreply.github.com>
2021-12-03 17:31:30 -0800

    + Hello text, +const key, tests green

--- lib/screens/root/home_page.dart
+++ lib/screens/root/home_page.dart
@@ -2,8 +2,11 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+
+  const HomePage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Text('Hello, friend.');
   }
 }
